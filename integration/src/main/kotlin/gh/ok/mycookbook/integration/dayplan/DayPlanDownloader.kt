1cbf10e3fe7fea54a97512923c87179cc98cd74e
Olga <olga.pozn@gmail.com>
2020-11-08 21:46:11 +0100

    parse html

--- integration/src/main/kotlin/gh/ok/mycookbook/integration/dayplan/DayPlanDownloader.kt
+++ integration/src/main/kotlin/gh/ok/mycookbook/integration/dayplan/DayPlanDownloader.kt
@@ -14,11 +14,13 @@ import pl.mfcookbook.core.services.DayPlanPageService
 import java.time.LocalDate
 
 
-class DayPlanDownloader(private val loginService: DayPlanLoginService,
-                        private val dayPlanPageService: DayPlanPageService,
-                        private val dayPlanRepository: IDayPlanRepository) : IDayPlanDownloader {
+class DayPlanDownloader(
+    private val loginService: DayPlanLoginService,
+    private val dayPlanPageService: DayPlanPageService,
+    private val dayPlanRepository: IDayPlanRepository
+) : IDayPlanDownloader {
 
-   override fun downloadDayPlansForDates(from: LocalDate, to: LocalDate) {
+    override fun downloadDayPlansForDates(from: LocalDate, to: LocalDate) {
         val driver = ChromeDriver()
         loginService.login(driver)
         var start = from
@@ -40,58 +42,57 @@ class DayPlanDownloader(private val loginService: DayPlanLoginService,
         // read meals
         doc.getElementsByClass("meal").forEach {
 
-            val mealId = it.attributes().get("name") // meal id
-            println(mealId)
+            val mealId = it.attributes().get("name")
+            it.children()[0].children()
+                .filter { it.hasClass("row") || it.hasClass("d-none d-sm-block") }
+                .filter { !it.hasClass("mt-5 row") }
+                .forEachIndexed { index, mealElement ->
+                    if (isItMealSummary(mealElement)) processSummary(mealElement) else processDish(mealElement)
+                }
+
+        }
+
+    }
+
+    private fun processSummary(mealElement: Element) {
+        println(mealElement.text())
+    }
+
+    private fun isItMealSummary(mealElement: Element): Boolean {
+        return mealElement.hasClass("d-none d-sm-block")
+    }
+
+    private fun processDish(mealElement: Element) {
+        mealElement.children().forEachIndexed { index, element ->
+            if (index == 0)
+                println(element.getElementsByClass("dishName").text())
+            processDishIngredients(element.children().filter { it.hasClass("ingredients") })
+            if (index == 1) processDescription(element)
+        }
+    }
+
+    private fun processDescription(element: Element) {
+        println(element.text())
+    }
+
+    private fun processDishIngredients(ingredientLists: List<Element>) {
+        ingredientLists.forEach {
             it.children().forEach {
-                val numOfChildren = it.children().filter { !it.text().isBlank() }.size
-                it.children().filter { !it.text().isBlank() }.forEachIndexed{ index, el ->
-                    if (index == 1) println("meal name and summary: ${el.text()}")
-                    if (index > 1 && index < numOfChildren - 1) processDish(el)
+                val amount = it.children()[0].text()
+                val product = it.textNodes().filter { !it.isBlank }.first().text()
+                println("$product $amount")
+
+                val substitutes = it.getElementsByClass("collapse")[0].getElementsByTag("li");
+                if (!substitutes.isEmpty())
+                    println("Substitutes")
+                substitutes.forEach {
+                    val amountSub = it.children()[0].text()
+                    val productSub = it.text().replace(amountSub, "")
+                    println("   $productSub $amountSub")
                 }
             }
-
-
-//
-//            val dishesNames = it.getElementsByClass("dishName") // list of dishes' names
-//            val ingredients = it.getElementsByClass("ingredients")
-//            showIngredients(ingredients)
-//            it.getElementsByClass("ingredients")[0].getElementsByTag("li")[0].textNodes().filter { !it.isBlank }.first().text() // 2nd ingredient name
-//            it.getElementsByClass("ingredients")[0].getElementsByTag("li")[0].getElementsByTag("span")[0].text() // quantity of first ingredient
-//
-//            it.getElementsByClass("");
-
         }
-        // read dishes
-                // read product lists
-                    // read substitutes
-                // read description
-        // read day summary
-        doc.getElementsByClass("summary")
 
-
-
-//        val sections = fileContent.split("<section class=").filter { it.startsWith("\"meal") || it.startsWith("\"summary") }
-//        sections.forEach {
-//            val doc: Document = Jsoup.parse("<section class=$it")
-//            println(doc.body().text())
-//        }
-    }
-
-    private fun processDish(el: Element) {
-        el.children().forEach {
-            println(it.text())
-        }
-    }
-
-    private fun showIngredients(ingredients: Elements) {
-        ingredients.forEach {
-            it.getElementsByTag("li").forEach {
-                val ing = it.textNodes().filter { !it.isBlank }
-                val name = if (ing.isNotEmpty()) ing.first().text() else "tu nie ma składnika"
-                val quantity = it.getElementsByTag("span")[0].text()
-                println("$name: $quantity")
-            }
-        }
     }
 
 
